e110eea102278022c1005741bad8750f769fa165
Pavel Ivanov <ivpavig@gmail.com>
2014-12-06 21:33:00 +0300

    Minor fixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
   root 'main#index'
-
-  # Main application page
   get 'app' => 'main#app'
 
   resources :courses do
-    resources :course_items
+    resources :course_items, path: 'items'
   end
 end
