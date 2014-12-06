2e8971a70df1afa6e240bbf0573d259161c65d38
Sergey Smagin <smaginsergey1310@gmail.com>
2014-12-06 18:30:30 +0300

    Woking on forking and merging

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,4 +8,20 @@ class Course
 
   belongs_to :user
   has_many :course_items, dependent: :destroy
+
+  def fork(user_id)
+    Course.create(user_id: user_id, duration: duration, parent_id: id,
+                  description: description, course_items: course_items)
+  end
+
+  def pull_request
+    merged_already = parent.course_items.where(user_id: id)
+    items = course_items.where(user_id: id) - merged_already
+    parent.suggested_items += items
+    parent.update
+  end
+
+  def parent
+    Course.find parent_id
+  end
 end
