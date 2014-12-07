98bb791e653326b3a87d74ff80c7916134e36da9
budmitr <budmitr@gmail.com>
2014-12-07 20:14:18 +0300

    Improved display of course item types

--- app/models/course_item.rb
+++ app/models/course_item.rb
@@ -2,13 +2,11 @@ class CourseItem
   include Mongoid::Document
 
   field :name, type: String
-  field :description, type: String
   field :url, type: String
   field :type, type: String # TODO: enum?
   field :duration, type: Float
   
   validates :name, presence: true
-  validates :description, presence: true
   validates :url, presence: true
   validates :type, presence: true
   validates :duration, presence: true, numericality: {greater_than: 0}
@@ -19,7 +17,7 @@ class CourseItem
   default_scope ->{ asc(:id) }
 
   def fork(course_id)
-    ci = CourseItem.create(name: name, description: description, url: url,
+    ci = CourseItem.create(name: name, url: url,
                            type: type, course_id: course_id)
     ItemProgress.create(learned: false, course_item_id: ci.id,
                         course_id: course_id)
